027210c8983cef45d39259a1ec28ef7f2bdbba34
Jeff Kolesky <jeff@kolesky.com>
2013-02-10 18:12:16 -0800

    Fixes Prefices example

--- examples/Prefices.hs
+++ examples/Prefices.hs
@@ -7,9 +7,8 @@ import Data.List (inits, sort, nub)
 
 pMap :: Mapper String String
 pMap line = [(prefix, line) | prefix <- tail $ inits line]
-pMap line = [prx ++ " " ++ line | prx <- tail $ inits line]
 
-pReduce :: Reduce String String String String
+pReduce :: Reducer String String String String
 pReduce prx wrds = [(prx, unwords $ sort $ nub wrds)]
 
 main = mrMain pMap pReduce
