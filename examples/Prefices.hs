0f22cc6fad79ff69eb4761026b927a47a55821d4
Jeff Kolesky <jeff@kolesky.com>
2013-02-15 10:03:46 -0800

    Uses right type for prefices job

--- examples/Prefices.hs
+++ examples/Prefices.hs
@@ -8,8 +8,8 @@ import Data.List (inits, sort, nub)
 pMap :: Mapper String String
 pMap line = [(prefix, line) | prefix <- tail $ inits line]
 
-pReduce :: Reducer String String String String
-pReduce prx wrds = [(prx, unwords $ sort $ nub wrds)]
+pReduce :: Reducer String String String [String]
+pReduce prx wrds = [(prx, sort $ nub wrds)]
 
 main = mrMain pMap pReduce
 
