51ff97fbb175db82d0c1d92813dfa557c75e2d60
Jeff Kolesky <jeff@kolesky.com>
2013-02-15 09:46:11 -0800

    Fixes map task of reverse directed graph

--- examples/ReverseDirectedGraph.hs
+++ examples/ReverseDirectedGraph.hs
@@ -1,25 +1,30 @@
 
 -- Takes a directed graph and reverses every edge in it.
 -- The directed graph is expected to be described as follows.
--- Each line corrisponds to one node in the graph. The line
+-- Each line corresponds to one node in the graph. The line
 -- begins with the name of the node followed by a space, and
 -- is followed by the names of adjacent nodes, separated by
 -- spaces. The output graph is in the same format.
 
 module Main where
 
-import Hadoop.MapReduce (mrMain, Mapper, Reducer)
+import Hadoop.MapReduce (mapMain, Mapper, Reducer)
 
+-- input line is [origin, destA, destB, destC,...]
+-- emit [(destA, origin), (destB, origin), (destC, origin),...]
 wfMap :: Mapper String String
 wfMap line =
-    let node = key line in
+    let nodes = words line in
+    let origin = head nodes in
     do
-        adjacentNode <- words $ value line
-        return $ adjacentNode ++ " " ++ node
+        adjacentNode <- tail nodes
+        return $ (adjacentNode, origin)
 
-wfReduce :: Reduce
+{-
+wfReduce :: Reducer String String String String
 wfReduce key values =
     return $ key ++ " " ++ (unwords values)
+    -}
 
-main = mrMain wfMap wfReduce
+main = mapMain wfMap -- wfReduce
 
